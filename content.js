99b653853f579da58e64f935b627cf1ec95a6ea2
David Xiao <avidxiao@gmail.com>
2021-01-07 20:17:13 -0800

    detect when user makes a time skip in youtube video

--- content.js
+++ content.js
@@ -1,10 +1,28 @@
+// CONSTANTS
+const THRESHOLD = 2;
+
 // Retrieve the Youtube Player
 const youtubePlayer = document.getElementsByClassName(
   'video-stream html5-main-video'
 )[0];
 
 // Chrome Extension API
-// Listen for message from popup.js
+// Listen for message from background.js
+// Receives adjusted time if it is not in sync.
 chrome.runtime.onMessage.addListener((req) => {
   youtubePlayer.currentTime = req;
+});
+
+// Wait for page to finish loading
+window.addEventListener('load', () => {
+  let prev_time = youtubePlayer.currentTime;
+  // Look for instance when user makes a significant change to current time
+  // Detects this by comparing current time to previous time half a second ago.
+  setInterval(() => {
+    if (Math.abs(youtubePlayer.currentTime - prev_time) > THRESHOLD) {
+      alert('gg');
+    }
+    console.log(youtubePlayer.currentTime - prev_time);
+    prev_time = youtubePlayer.currentTime;
+  }, 500);
 });
--- popup/popup.js
+++ popup/popup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('button').addEventListener('click', onclick, false);
 
   function onclick() {
-    chrome.runtime.sendMessage('_');
+    chrome.runtime.sendMessage('popup');
     // chrome.runtime.onMessage.addListener((req) => {
     //   button.innerText = req;
     // });
