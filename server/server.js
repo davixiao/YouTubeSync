f21400c231bad56af6359834dc2841302bdc80b1
David Xiao <avidxiao@gmail.com>
2021-01-07 22:56:08 -0800

    fixed bug where content script would not be injected when user navigates with links on youtube.com. Some reorganizing of files as well

--- server/server.js
+++ server/server.js
@@ -6,12 +6,30 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// Page display
+app.get('/', (req, res) => {
+  res.send('Youtube Sync Server');
+});
+
+let sockets = [];
+
 // Run when client connects
 io.on('connection', (socket) => {
-  socket.emit('message', 'FROM THE SERVER');
+  socket.emit('message', 'CONNECTED TO SERVER');
 
-  socket.on('test', (msg) => {
-    console.log(msg);
+  socket.on('_skip', (payload) => {
+    //socket.broadcast.emit('server_skip', payload);
+    console.log('user skipped to', payload);
+  });
+
+  socket.on('_pause', (payload) => {
+    //socket.broadcast.emit('server_pause', payload);
+    console.log('paused?', payload);
+  });
+
+  socket.on('_disconnect', () => {
+    //console.log('user disconnected');
+    socket.disconnect();
   });
 });
 
