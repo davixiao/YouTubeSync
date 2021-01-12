d27ec9645d4a05cd2a0255c18c0945414a6d3054
David Xiao <avidxiao@gmail.com>
2021-01-12 12:00:53 -0800

    fixed CORS origin to be chrome extension. Narrowed chrome extension access from all URLs to just youtube

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'chrome-extension://kmgehjafppoflmeeimcegnacppejngca',
     methods: ['GET', 'POST'],
     credentials: true,
   },
